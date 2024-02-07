f26976f1ead073e4ef360a12a2fd001f42bc4074
Rupesh Yerra <brahmam.mekala123@gmail.com>
2024-02-07 19:28:56 +0530

    brahmam005

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "zone_id" {
+  default = "Z06234872TGVNTMOXNTKX"
+}
+
+variable "domain_name" {
+  default = "brahmam005aws.shop"
+}
