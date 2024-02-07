71627f8fd27419bc1f6430e045e8ab05e6d1c02c
Rupesh Yerra <brahmam.mekala123@gmail.com>
2024-02-07 18:31:28 +0530

    brahmam005

--- count/count.tf
+++ count/count.tf
@@ -1,8 +1,18 @@
 resource "aws_instance" "web"  {
-  count = 11
-  ami           = "var.ami_id" #devops-practice
+  count = 2 #count.index is a special variable given by terraform
+  ami           = var.ami_id #devops-practice
   instance_type = "t2.micro"
   tags = {
-    Name = "Web"
+    Name = var.instance_names[count.index]
   }
+}
+
+resource "aws_route53_record" "www" {
+  #count = 2
+  count = length(var.instance_names)
+  zone_id = var.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
+  type    = "A"
+  ttl     = 1
+  records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 }
