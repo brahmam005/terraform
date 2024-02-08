3abc57b6afffce8bca727abd8dac1b08d8684b00
Rupesh Yerra <brahmam.mekala123@gmail.com>
2024-02-08 19:55:45 +0530

    brahmam005

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.35.0" # AWS provider version, not terraform version
     }
   }
+
+  backend "s3" {
+    bucket = "brahmam005-remote-state"
+    key    = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "brahmam005-locking"
+  }
 }
 
 provider "aws" {
